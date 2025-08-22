068c4a597f20375a529ff5d719975c7e8a2c31cb
AbuTokio <bunyaminxkara@gmail.com>
2025-08-22 10:45:55 +0200

    slightly changed 'unfilter' functionality

--- src/main.ts
+++ src/main.ts
@@ -11,48 +11,48 @@ const html = {
   buttons: {
     typeButtons: [document.querySelector("#type-buttons"), {}] as [HTMLDivElement, Record<string, HTMLButtonElement>],
   },
-  main: document.querySelector("main") as HTMLDivElement,
+  main: [document.querySelector("main"), {}] as [HTMLDivElement, Record<string, HTMLButtonElement>],
 }
 
 const pokemon: Pokemon[] = await getPokemon()
 
 pokemon.forEach(async (pokemon) => {
-  html.main.appendChild(await displayPokemon(pokemon))
+  html.main[Element.THIS].appendChild(await displayPokemon(pokemon))
 })
 
-TYPES.forEach((type: PokemonType) => {
-  const typeButton = createButton(type, TYPE_COLORS[type])
-  html.buttons.typeButtons[Element.CHILD][type] = typeButton
-  html.buttons.typeButtons[Element.THIS].appendChild(typeButton)
-
-  typeButton.addEventListener("click", async () => {
-    btn.style.display = "initial"
-    html.main.innerHTML = ""
-    const filteredPokemon1: Pokemon[] = pokemon.filter((pok) => {
-      return pok.types[0].type.name === type
-    })
-
-    const filteredPokemon2: Pokemon[] = pokemon.filter((pok) => {
-      if (pok.types[1]) return pok.types[1].type.name === type
-    })
-
-    const filteredPokemon: Pokemon[] = [...filteredPokemon1, ...filteredPokemon2]
-
-    filteredPokemon.forEach(async (pokemon) => {
-      html.main.appendChild(await displayPokemon(pokemon))
+function addButtons() {
+  const btn = createButton("all", "#000")
+  html.buttons.typeButtons[Element.CHILD]["all"] = btn
+  html.buttons.typeButtons[Element.THIS].appendChild(btn)
+  btn.addEventListener("click", () => {
+    html.main[Element.THIS].innerHTML = ""
+    pokemon.forEach(async (pokemon) => {
+      html.main[Element.THIS].appendChild(await displayPokemon(pokemon))
     })
   })
-})
 
-const btn = createButton("X", "#000")
-html.buttons.typeButtons[Element.CHILD]["X"] = btn
-html.buttons.typeButtons[Element.THIS].appendChild(btn)
-btn.addEventListener("click", () => {
-  html.main.innerHTML = ""
-  pokemon.forEach(async (pokemon) => {
-    html.main.appendChild(await displayPokemon(pokemon))
+  TYPES.forEach((type: PokemonType) => {
+    const typeButton = createButton(type, TYPE_COLORS[type])
+    html.buttons.typeButtons[Element.CHILD][type] = typeButton
+    html.buttons.typeButtons[Element.THIS].appendChild(typeButton)
+
+    typeButton.addEventListener("click", async () => {
+      html.main[Element.THIS].innerHTML = ""
+      const filteredPokemonType1: Pokemon[] = pokemon.filter((pok) => {
+        return pok.types[0].type.name === type
+      })
+
+      const filteredPokemonType2: Pokemon[] = pokemon.filter((pok) => {
+        if (pok.types[1]) return pok.types[1].type.name === type
+      })
+
+      const filteredPokemon: Pokemon[] = [...filteredPokemonType1, ...filteredPokemonType2]
+
+      filteredPokemon.forEach(async (pokemon) => {
+        html.main[Element.THIS].appendChild(await displayPokemon(pokemon))
+      })
+    })
   })
-  btn.style.display = "none"
-})
+}
 
-btn.style.display = "none"
+addButtons()
